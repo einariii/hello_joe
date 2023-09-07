5d0eb6883a0ab820025cb70134e8308a8faf3154
Einar Engstrom <meengst@gmail.com>
2023-09-07 14:53:03 -0400

    adding favorite erlang program

---
+++ fav1.erl
@@ -0,0 +1,29 @@
+-module(fav1).
+-export([universal_server/0, factorial_server/0, factorial/1, test/0]).
+
+universal_server() ->
+  receive
+    {become, F} ->
+      F()
+  end.
+
+factorial_server() ->
+  receive 
+    {From, N} ->
+      From ! factorial(N),
+      factorial_server()
+  end.
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
+
+test() ->
+  Pid = spawn(fun universal_server/0),
+  Pid ! {become, fun factorial_server/0},
+  Pid ! {self(), 50},
+  receive 
+    X -> X
+  end.
+
+
+
